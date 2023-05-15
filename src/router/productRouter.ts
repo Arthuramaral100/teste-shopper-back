ed575b24dd3e2ce2bd13193522b861af3f37dbd0
Arthuramaral100 <arthuramaral57@gmail.com>
2023-05-15 19:45:55 -0300

    comecei a configuração do controller e business da tabela products

--- src/router/productRouter.ts
+++ src/router/productRouter.ts
@@ -1,4 +1,12 @@
 import express from "express";
+import { ProductController } from "../controller/ProductController";
 
 export const productRouter = express.Router()
 
+const productController = new ProductController(
+
+)
+
+productRouter.get("/", productController.getProducts)
+productRouter.get("/verify")
+productRouter.put("/edit")
